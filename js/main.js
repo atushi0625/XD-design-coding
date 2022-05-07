caf4005c8187e483eee28238b32a2ff21fc983e6
atushi0625 <atushi720@gmail.com>
2022-05-08 07:07:19 +0900

    responnsive.scssとmain.scss変更

--- js/main.js
+++ js/main.js
@@ -9,13 +9,13 @@
      const close = document.getElementById('close-modal');
 
 
-     open.addEventListener('click', ()=>{
-         modal.classList.add('is-show');
+     open.addEventListener('click', () => {
+          modal.classList.add('is-show');
      });
-     close.addEventListener('click', ()=>{
-         modal.classList.remove('is-show');
+     close.addEventListener('click', () => {
+          modal.classList.remove('is-show');
      });
-     mask.addEventListener('click', ()=>{
+     mask.addEventListener('click', () => {
           close.click();
      });
 }
@@ -25,7 +25,7 @@
      const hamburger = document.getElementById('hamburger');
      const menu = document.getElementById('header-menu');
 
-     hamburger.addEventListener('click', ()=>{
+     hamburger.addEventListener('click', () => {
           hamburger.classList.toggle('is-active');
           menu.classList.toggle('open');
      });
@@ -34,36 +34,37 @@
 // スクロールヘッダー
 
 {
-     
-     window.addEventListener("scroll",function(){
+
+     window.addEventListener("scroll", function () {
           const scroll = document.documentElement.scrollTop;
           const header = document.querySelector('.header');
-          const menuItem = document.querySelectorAll('.header-menu__item');
+          const menuItem = document.querySelectorAll('.header-menu__link');
           const bar = document.querySelectorAll('.hamburger__bar');
           const ImgChange = document.querySelector('.header-logo-img');
 
-               if(scroll > 0){
-                    ImgChange.src = ImgChange.dataset.image ;
-                    header.classList.add('change-header-color');
-                    for(let i = 0; i < bar.length; i++){
-                         bar[i].classList.add('change-bar-color');
-                    }
-                    for (let i = 0; i < menuItem.length; i++) {
-                         menuItem[i].classList.add('change-menu-color');
-                    }
-               }else{
-                    ImgChange.src = ImgChange.dataset.imageundo;
-                    header.classList.remove('change-header-color');
-                    for (let i = 0; i < bar.length; i++) {                    
-                         bar[i].classList.remove('change-bar-color');
-                    }
-                    for (let i = 0; i < menuItem.length; i++) {
-                         menuItem[i].classList.remove('change-menu-color');
-                    }
+          if (scroll > 0) {
+               ImgChange.src = ImgChange.dataset.image;
+               header.classList.add('change-header-color');
+               for (let i = 0; i < bar.length; i++) {
+                    bar[i].classList.add('change-bar-color');
                }
-            
+               for (let i = 0; i < menuItem.length; i++) {
+                    menuItem[i].classList.add('change-menu-color');
+               }
+          } else {
+               ImgChange.src = ImgChange.dataset.imageundo;
+               header.classList.remove('change-header-color');
+               for (let i = 0; i < bar.length; i++) {
+                    bar[i].classList.remove('change-bar-color');
+               }
+               for (let i = 0; i < menuItem.length; i++) {
+                    menuItem[i].classList.remove('change-menu-color');
+               }
+          }
+
      });
 }
+
 
 // タブメニュー
 {
@@ -71,49 +72,44 @@
      const tabsMenu = document.querySelectorAll(".tab-menu");
 
      for (let i = 0; i < tabs.length; i++) {
-     tabs[i].addEventListener("click",function(e){
-          e.preventDefault();
+          tabs[i].addEventListener("click", function (e) {
+               e.preventDefault();
 
-     for (let j = 0; j < tabs.length; j++) {
-          tabs[j].classList.remove('tab-border');
-     }
-     for (let j = 0; j < tabsMenu.length; j++) {
-          tabsMenu[j].classList.remove('tab-active');
-     }
+               for (let j = 0; j < tabs.length; j++) {
+                    tabs[j].classList.remove('tab-border');
+               }
+               for (let j = 0; j < tabsMenu.length; j++) {
+                    tabsMenu[j].classList.remove('tab-active');
+               }
 
-          this.classList.add("tab-border");
-          tabsMenu[i].classList.add("tab-active");
-     }); 
+               this.classList.add("tab-border");
+               tabsMenu[i].classList.add("tab-active");
+          });
      }
 }
 
 // フワッとスクロールアニメーション
 
 {
-  const targetElements = document.querySelectorAll(".animationTarget");
-  document.addEventListener("scroll", function(){
-       targetElements.forEach(function(targetElement){
-          const getElementDistance = targetElement.getBoundingClientRect().top + targetElement.clientHeight * 0.6;
-          if (window.innerHeight > getElementDistance) {
-               targetElement.classList.add("show");
-          }else{
-               targetElement.classList.remove("show");
+     const targetElements = document.querySelectorAll(".animationTarget");
+     document.addEventListener("scroll", function () {
+          targetElements.forEach(function (targetElement) {
+               const getElementDistance = targetElement.getBoundingClientRect().top + targetElement.clientHeight * 0.4;
+               if (window.innerHeight > getElementDistance) {
+                    targetElement.classList.add("show");
+               }
+               return
+          });
 
-          }
-       });
-
-  });
+     });
 }
 
 
 // カレンダー
-flatpickr(".flatpickr", { 
-     locale:"ja",
+flatpickr(".flatpickr", {
+     locale: "ja",
      minDate: "today",
-	mode: "range",
-	dateFormat: "Y-m-d",
+     mode: "range",
+     dateFormat: "Y-m-d",
 
 });
-
-
-
